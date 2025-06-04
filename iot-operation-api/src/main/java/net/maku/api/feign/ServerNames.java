94aacd64e8f97c7d8835de389ef9231d35933391
oranges <58148430@qq.com>
2025-06-04 16:52:20 +0800

    初始化项目

---
+++ iot-operation-api/src/main/java/net/maku/api/feign/ServerNames.java
@@ -0,0 +1,14 @@
+package net.maku.api.feign;
+
+/**
+ * 服务名称集合
+ *
+ * @author 阿沐 babamu@126.com
+ * <a href="https://maku.net">MAKU</a>
+ */
+public interface ServerNames {
+    /**
+     * iot-operation-system 服务名
+     */
+    String SYSTEM_SERVER_NAME = "iot-operation-system";
+}
