90e275779c47ee5d308284b3507837f9ee02d2db
ctynt <2597704619@qq.com>
2025-06-11 17:33:54 +0800

    用户分配设备完成

---
+++ iot-operation-new/src/main/java/net/maku/mealUser/convert/TPackageConvert.java
@@ -0,0 +1,32 @@
+package net.maku.mealUser.convert;
+
+import net.maku.mealUser.entity.TPackageEntity;
+import net.maku.mealUser.vo.TPackageVO;
+import net.maku.mealUser.vo.TPackageExcelVO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+/**
+ * 套餐管理
+ *
+ * @author cry babamu@126.com
+ * <a href="https://maku.net">MAKU</a>
+ */
+@Mapper
+public interface TPackageConvert {
+    TPackageConvert INSTANCE = Mappers.getMapper(TPackageConvert.class);
+
+    TPackageEntity convert(TPackageVO vo);
+
+    TPackageVO convert(TPackageEntity entity);
+
+    List<TPackageVO> convertList(List<TPackageEntity> list);
+
+    List<TPackageEntity> convertList2(List<TPackageVO> list);
+
+    List<TPackageExcelVO> convertExcelList(List<TPackageEntity> list);
+
+    List<TPackageEntity> convertExcelList2(List<TPackageExcelVO> list);
+}
